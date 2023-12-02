9c4b518ef22c79a7e459b8126b98314896a8db71
Alumsdesigns <mtaylo60@jaguarlandrover.com>
2023-12-02 21:55:58 +0000

    added z-index 11 to nav bar so skills don't bleed thru

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-black bg-opacity-100">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-black bg-opacity-100 z-[11]">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={'/'}
