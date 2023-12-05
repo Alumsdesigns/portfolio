9f47c38ccc8b1d7648f998e8155a937af88f50fe
Alumsdesigns <mtaylo60@jaguarlandrover.com>
2023-12-05 18:23:33 +0000

    updated the 1 next Image with priority

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,13 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-white border-l-transparent border-r-transparent text-white bg-black">
       <div className="container p-12 flex justify-between items-center">
-        <Image src="/images/logofinal.png" alt="Logo" width={50} height={50} />{' '}
+        <Image
+          src="/images/logofinal.png"
+          alt="Logo"
+          width={50}
+          height={50}
+          priority
+        />{' '}
         <div className="text-slate-100">
           <p>&copy; {new Date().getFullYear()} Damaris Alum</p>{' '}
           <p>Designed by Damaris Alum</p>
